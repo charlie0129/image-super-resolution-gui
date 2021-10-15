2348449da4610c1521e8ee034021d9ecaee87c7f
Charlie Chiang <charlie_c_0129@outlook.com>
2021-10-15 10:48:13 +0800

    chore: supress some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,31 @@
 module.exports = {
-  extends: 'erb',
+  extends: "erb",
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
-    'import/no-extraneous-dependencies': 'off',
+    "import/no-extraneous-dependencies": "off",
+    "no-nested-ternary": "warn",
+    "react/jsx-props-no-spreading": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "@typescript-eslint/no-shadow": "warn",
   },
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
-    project: './tsconfig.json',
+    sourceType: "module",
+    project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
     createDefaultProgram: true,
   },
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
       node: {},
       webpack: {
-        config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
+        config: require.resolve("./.erb/configs/webpack.config.eslint.ts"),
       },
     },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
   },
 };
